d94ca424be1c8d76f309e14f54c0848b4fe1a5ec
podverta <podverta@yandex.ru>
2022-03-21 07:49:28 +0300

    added models Item and Sample

--- bb/models.py
+++ bb/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    """items for embroidery"""
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        """return name"""
+        return self.name
+
+class Sample(models.Model):
+    type = models.ForeignKey(Item, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, unique=True)
+    color = models.CharField(max_length=20)
+    size = models.CharField(max_length=20)
+    quantity = models.IntegerField
+    comments = models.TextField
+
+    def __str__(self):
+        return self.name
