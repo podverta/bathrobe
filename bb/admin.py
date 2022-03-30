cbdb6d581380ea3424995a23d6d50e09e55a2a23
podverta <podverta@yandex.ru>
2022-03-30 18:23:08 +0300

    fixed db_field for bathrobe, towel and toys.

--- bb/admin.py
+++ bb/admin.py
@@ -1,22 +1,37 @@
-from django import forms
+from django.forms import ModelChoiceField
 from django.contrib import admin
 
 
 from .models import *
 
 
+class TowelAdmin(admin.ModelAdmin):
 
-class BathrobesCategoryChoiceField(forms.ModelChoiceField):
-    pass
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'type_name':
+            pass
+            return ModelChoiceField(Item.objects.filter(name='Полотенце'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class ToysAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'type_name':
+            pass
+            return ModelChoiceField(Item.objects.filter(name='Игрушка'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 class BathrobeAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'item':
-            return BathrobesCategoryChoiceField(Category.objects.filter(name='Халат'))
+        if db_field.name == 'type_name':
+            pass
+            return ModelChoiceField(Item.objects.filter(name='Халат'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(Item)
-admin.site.register(Toys)
-admin.site.register(Towel)
+admin.site.register(Toys, ToysAdmin)
+admin.site.register(Towel, TowelAdmin)
 admin.site.register(Bathrobe, BathrobeAdmin)
